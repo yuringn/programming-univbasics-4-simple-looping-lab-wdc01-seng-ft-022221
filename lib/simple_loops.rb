3705f33e30c6c0ee2a1226accbf3930422a656b6
Yuri Ng <yuringuyen312@gmail.com>
2021-02-06 06:21:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,7 @@
 # Write your methods here
 def loop_message_five_times(string)
-  puts string * 5
+5.times do
+  puts string
 end
-loop_message_five_times("Hello")
+end
+loop_message_five_times(" Hello ")
